478b304023cc1aed58f3b08d9fbdef1943bd3e9c
TheSilkMiner <thesilkminer@outlook.com>
2025-08-15 18:15:56 +0200

    Fix generic error in RuleState
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/invoke/RuleState.kt
+++ script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/invoke/RuleState.kt
@@ -3,10 +3,10 @@ package net.thesilkminer.babelk.script.api.invoke
 interface RuleState {
     val ruleName: String?
 
-    operator fun <T> get(key: RuleStateKey<T>): T?
-    operator fun <T> set(key: RuleStateKey<T>, value: T)
+    operator fun <T : Any> get(key: RuleStateKey<T>): T?
+    operator fun <T : Any> set(key: RuleStateKey<T>, value: T)
 
-    fun <T> getOrPut(key: RuleStateKey<T>, default: () -> T): T
-    fun <T> update(key: RuleStateKey<T>, updater: (T?) -> T): T
-    fun <T> updateIfPresent(key: RuleStateKey<T>, updater: (T) -> T): T?
+    fun <T : Any> getOrPut(key: RuleStateKey<T>, default: () -> T): T
+    fun <T : Any> update(key: RuleStateKey<T>, updater: (T?) -> T): T
+    fun <T : Any> updateIfPresent(key: RuleStateKey<T>, updater: (T) -> T): T?
 }
