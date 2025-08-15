180720c53e72a1bd442aa64b8830e12ed7e4a8b9
TheSilkMiner <thesilkminer@outlook.com>
2025-08-15 09:54:43 +0200

    Fix collection requiring construction of named object directly
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/collection/MutableNamedObjectCollection.kt
+++ script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/collection/MutableNamedObjectCollection.kt
@@ -3,6 +3,6 @@ package net.thesilkminer.babelk.script.api.collection
 import net.thesilkminer.babelk.script.api.NamedObject
 import net.thesilkminer.babelk.script.api.provider.NamedObjectProvider
 
-interface MutableNamedObjectCollection<E : NamedObject> : NamedObjectCollection<E> {
-    fun register(name: String, creator: () -> E): NamedObjectProvider<E>
+interface MutableNamedObjectCollection<out E : NamedObject, in B> : NamedObjectCollection<E> {
+    fun register(name: String, creator: () -> B): NamedObjectProvider<E>
 }
