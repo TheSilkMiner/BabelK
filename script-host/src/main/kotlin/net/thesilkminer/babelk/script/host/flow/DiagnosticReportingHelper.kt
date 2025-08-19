b920791ec50ff1ad388f0f1a4e3cb4f05255d600
TheSilkMiner <thesilkminer@outlook.com>
2025-08-19 18:36:18 +0200

    Move diagnostic reporting to separate helper
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ script-host/src/main/kotlin/net/thesilkminer/babelk/script/host/flow/DiagnosticReportingHelper.kt
@@ -0,0 +1,68 @@
+@file:JvmName("DiagnosticReportingHelper")
+
+package net.thesilkminer.babelk.script.host.flow
+
+import net.thesilkminer.babelk.script.host.Log
+import java.io.File
+import java.util.Locale
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.valueOr
+import kotlin.text.substringAfterLast
+
+internal fun <R> ResultWithDiagnostics<R>.reportDiagnostics(log: Log): ResultWithDiagnostics<R> {
+    this.reports.forEach { (code, message, severity, sourcePath, location, exception) ->
+        log.withSeverity(severity, exception) {
+            buildString {
+                append("  [")
+                append(severity.name.let { "${it[0]}${it.substring(1).lowercase(Locale.ENGLISH)}" })
+                append(" 0x")
+                append(code.toString(radix = 16).padStart(16, '0'))
+                append(']')
+
+                if (sourcePath != null) {
+                    append(" in ")
+                    append(sourcePath.substringAfterLast(File.pathSeparatorChar))
+                }
+
+                if (location != null) {
+                    append(" at ")
+                    append(location.start.line)
+                    append(':')
+                    append(location.start.col)
+                }
+
+                append(':')
+                append(message)
+
+                if (exception != null) {
+                    append(" (this caused a ")
+                    append(exception::class.qualifiedName)
+                    append(": ")
+                    append(exception.message ?: "<no error message>")
+                    append(')')
+                }
+            }
+        }
+    }
+    return this
+}
+
+internal fun <R> ResultWithDiagnostics<R>.rethrowOnError(log: Log, messageProvider: () -> String): R {
+    return this.valueOr { _ ->
+        val message = messageProvider()
+        val exception = RuntimeException(message)
+        log.error(exception) { message }
+        throw exception
+    }
+}
+
+private fun Log.withSeverity(severity: ScriptDiagnostic.Severity, throwable: Throwable?, messageProvider: () -> String) {
+    when (severity) {
+        ScriptDiagnostic.Severity.DEBUG -> this.debug(throwable, messageProvider)
+        ScriptDiagnostic.Severity.INFO -> this.info(throwable, messageProvider)
+        ScriptDiagnostic.Severity.WARNING -> this.warn(throwable, messageProvider)
+        ScriptDiagnostic.Severity.ERROR -> this.error(throwable, messageProvider)
+        ScriptDiagnostic.Severity.FATAL -> this.error(throwable) { "FATAL: ${messageProvider()}" }
+    }
+}
--- script-host/src/main/kotlin/net/thesilkminer/babelk/script/host/flow/compilation/HostCompiler.kt
+++ script-host/src/main/kotlin/net/thesilkminer/babelk/script/host/flow/compilation/HostCompiler.kt
@@ -11,12 +11,12 @@ import net.thesilkminer.babelk.script.host.interop.ScriptCollection
 import net.thesilkminer.babelk.script.host.flow.LoadableScript
 import net.thesilkminer.babelk.script.host.flow.extractGrammarNameFromScriptNameOrNull
 import net.thesilkminer.babelk.script.host.flow.isValidGrammarName
+import net.thesilkminer.babelk.script.host.flow.reportDiagnostics
+import net.thesilkminer.babelk.script.host.flow.rethrowOnError
 import net.thesilkminer.babelk.script.host.flow.toGrammarClassName
 import net.thesilkminer.babelk.script.host.flow.verifyValidGrammarName
 import net.thesilkminer.babelk.script.host.withinCoroutine
-import java.io.File
 import java.nio.file.Paths
-import java.util.Locale
 import kotlin.reflect.KClass
 import kotlin.script.experimental.api.CompiledScript
 import kotlin.script.experimental.api.ResultWithDiagnostics
@@ -27,7 +27,6 @@ import kotlin.script.experimental.api.defaultIdentifier
 import kotlin.script.experimental.api.foundAnnotations
 import kotlin.script.experimental.api.makeFailureResult
 import kotlin.script.experimental.api.refineConfiguration
-import kotlin.script.experimental.api.valueOr
 import kotlin.script.experimental.api.with
 import kotlin.script.experimental.jvm.dependenciesFromClassContext
 import kotlin.script.experimental.jvm.jvm
@@ -54,7 +53,7 @@ internal fun ScriptCollection.compile(): Collection<LoadableScript> {
 private fun Script.compile(): LoadableScript {
     log.info { "Compiling script ${this.name}" }
     val builtinName = this.extractGrammarNameFromScriptNameOrNull()
-    val script = this.compileWithResult(builtinName).reportDiagnostics().rethrowOnError(this)
+    val script = this.compileWithResult(builtinName).reportDiagnostics(log).rethrowOnError(this)
     return LoadableCompiledScript(script)
 }
 
@@ -95,60 +94,8 @@ private fun Script.compileWithResult(builtinGrammarName: String?): ResultWithDia
 
 private operator fun <T> ScriptCollectedData?.get(key: PropertiesCollection.Key<T>): T? = this?.let { it[key] }
 
-private fun ResultWithDiagnostics<CompiledScript>.reportDiagnostics(): ResultWithDiagnostics<CompiledScript> {
-    this.reports.forEach { (code, message, severity, sourcePath, location, exception) ->
-        log.withSeverity(severity, exception) {
-            buildString {
-                append("  [")
-                append(severity.name.let { "${it[0]}${it.substring(1).lowercase(Locale.ENGLISH)}" })
-                append(" 0x")
-                append(code.toString(radix = 16).padStart(16, '0'))
-                append(']')
-
-                if (sourcePath != null) {
-                    append(" in ")
-                    append(sourcePath.substringAfterLast(File.pathSeparatorChar))
-                }
-
-                if (location != null) {
-                    append(" at ")
-                    append(location.start.line)
-                    append(':')
-                    append(location.start.col)
-                }
-
-                append(':')
-                append(message)
-
-                if (exception != null) {
-                    append(" (this caused a ")
-                    append(exception::class.qualifiedName)
-                    append(": ")
-                    append(exception.message ?: "<no error message>")
-                    append(')')
-                }
-            }
-        }
-    }
-    return this
-}
-
 private fun ResultWithDiagnostics<CompiledScript>.rethrowOnError(script: Script): CompiledScript {
-    return this.valueOr { _ ->
-        val message = "Compilation errors have occurred while attempting to compile script ${script.name}: please check the compiler output"
-        log.error { message }
-        throw RuntimeException(message)
-    }
-}
-
-private fun Log.withSeverity(severity: ScriptDiagnostic.Severity, throwable: Throwable?, messageProvider: () -> String) {
-    when (severity) {
-        ScriptDiagnostic.Severity.DEBUG -> this.debug(throwable, messageProvider)
-        ScriptDiagnostic.Severity.INFO -> this.info(throwable, messageProvider)
-        ScriptDiagnostic.Severity.WARNING -> this.warn(throwable, messageProvider)
-        ScriptDiagnostic.Severity.ERROR -> this.error(throwable, messageProvider)
-        ScriptDiagnostic.Severity.FATAL -> this.error(throwable) { "FATAL: ${messageProvider()}" }
-    }
+    return this.rethrowOnError(log) { "Compilation errors have occurred while attempting to compile script ${script.name}: please check the compiler output" }
 }
 
 private fun lazyLibraries(block: MutableList<KClass<*>>.() -> Unit): Lazy<Array<String>> {
