37df9a0985726614ec87dc7e693a789555904fa7
TheSilkMiner <thesilkminer@outlook.com>
2025-08-25 17:44:50 +0200

    Fix missing patch module for cli
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -9,6 +9,7 @@ application {
     // TODO("Determine how to produce two distributions, one for J8 and one for J9+")
     mainModule = "net.thesilkminer.babelk.cli"
     mainClass = "net.thesilkminer.babelk.cli.Babelk"
+    applicationDefaultJvmArgs = listOf("--add-modules=jdk.unsupported")
 }
 
 multiRelease {
@@ -20,4 +21,18 @@ multiRelease {
 dependencies {
     implementation(project(":library"))
     implementation(libs.picocli)
+}
+
+tasks.named<JavaCompile>(sourceSets["java9"].compileJavaTaskName) {
+    options.javaModuleVersion = "${project.version}"
+    sourceSets.main
+        .map { it.output.classesDirs.files.joinToString(prefix = "${project.group}.cli=", separator = File.pathSeparator) }
+        .let { options.compilerArgumentProviders.add { listOf("--patch-module", it.get()) }}
+}
+
+tasks.named<ProcessResources>("processResources") {
+    val version = project.version
+    filesMatching("META-INF/babelk.version") {
+        expand("version" to version)
+    }
 }
