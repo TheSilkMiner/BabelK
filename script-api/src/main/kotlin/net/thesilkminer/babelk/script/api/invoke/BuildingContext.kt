c27dc6a348f6bcef91c1a066e8e3b4ad5ded7010
TheSilkMiner <thesilkminer@outlook.com>
2025-09-04 18:34:31 +0200

    Introduce DSL for capturing for easier definitions of captures
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/invoke/BuildingContext.kt
+++ script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/invoke/BuildingContext.kt
@@ -4,5 +4,5 @@ interface BuildingContext {
     fun append(literal: String)
     fun invoke(rule: InvokableRule)
 
-    fun capture(mode: CaptureMode = CaptureMode.RECURSE, block: (BuildingContext) -> Unit): BuildingEventList
+    fun capture(mode: CaptureMode = CaptureMode.RECURSE, block: (context: BuildingContext) -> Unit): BuildingEventList
 }
---
+++ script-api/src/main/kotlin/net/thesilkminer/babelk/script/api/invoke/BuildingContextCapturingDsl.kt
@@ -0,0 +1,48 @@
+@file:JvmName("BuildingContextCapturingDsl")
+
+package net.thesilkminer.babelk.script.api.invoke
+
+// Note: we are providing this DSL here instead of in the script-dsl package as this DSL is actually quite useful
+// for raw implementation of rules, rather than only for scripts
+
+interface BuildingContextCaptureDsl {
+    fun capture(block: (context: BuildingContext) -> Unit)
+    fun andThen(block: BuildingContext.(events: BuildingEventList) -> Unit)
+}
+
+private class ContextCapture(private val context: BuildingContext, private val mode: CaptureMode) : BuildingContextCaptureDsl {
+    private var capture: ((BuildingContext) -> Unit)? = null
+    private var andThen: (BuildingContext.(BuildingEventList) -> Unit)? = null
+
+    override fun capture(block: (context: BuildingContext) -> Unit) {
+        require(this.capture == null) { "Capture function has already been provided" }
+        this.capture = block
+    }
+
+    override fun andThen(block: BuildingContext.(events: BuildingEventList) -> Unit) {
+        require(this.andThen == null) { "Follow-up function has already been provided" }
+        this.andThen = block
+    }
+
+    operator fun invoke() {
+        val capture = requireNotNull(this.capture) { "Capture function has not been provided" }
+        val andThen = requireNotNull(this.andThen) { "Follow-up function has not been provided" }
+
+        with(this.context) {
+            val events = this.capture(this@ContextCapture.mode, capture)
+            this.andThen(events)
+        }
+    }
+}
+
+fun BuildingContext.capturing(mode: CaptureMode = CaptureMode.RECURSE, block: BuildingContextCaptureDsl.() -> Unit) {
+    ContextCapture(this, mode).apply(block)()
+}
+
+fun BuildingContextCaptureDsl.andThenWithAppends(block: BuildingContext.(literals: List<AppendLiteral>) -> Unit) {
+    this.andThen { block(it.filterAllAppends()) }
+}
+
+fun BuildingContextCaptureDsl.andThenWithConcatenation(separator: String = "", block: BuildingContext.(result: String) -> Unit) {
+    this.andThen { block(it.concatenateAppends(separator)) }
+}
