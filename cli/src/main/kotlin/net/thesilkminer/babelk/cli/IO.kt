18c65d59476616ae202331f8d17f1750caf74af1
TheSilkMiner <thesilkminer@outlook.com>
2025-09-16 13:47:32 +0200

    Rework logging to allow for exception printing
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- cli/src/main/kotlin/net/thesilkminer/babelk/cli/IO.kt
+++ cli/src/main/kotlin/net/thesilkminer/babelk/cli/IO.kt
@@ -65,12 +65,115 @@ internal fun Console.raiseError(errorMessage: String) {
     this.rawError("! $errorMessage")
 }
 
-@Suppress("UnusedReceiverParameter")
 internal fun Console.raiseException(throwable: Throwable, errorMessage: String) {
-    // TODO("Move logger formatting to a common method and use the same formatting here")
-    // TODO("Alternatively, move logger to use Console directly for exception formatting")
-    val logger = CliLoggerFactory().newLogger("Shell")
-    logger.error(throwable) { errorMessage }
+    this.log(throwable, true) { errorMessage }
+}
+
+internal fun Console.log(
+    throwable: Throwable?,
+    error: Boolean,
+    marker: Char? = null,
+    showStackTrace: Boolean = LoggingConfiguration.instance.showStackTraces,
+    messageProvider: () -> String
+) {
+    fun StackTraceElement.appendTo(target: MutableList<String>, indent: String) {
+        target += "$indent    at $this"
+    }
+
+    fun Throwable.appendNameMessage(target: MutableList<String>, header: String?, indent: String) {
+        target += "$indent${header ?: ""}${this::class.qualifiedName}: ${this.message ?: "<no error message>"}"
+    }
+
+    fun Throwable.appendAllEnclosed(target: MutableList<String>, seen: MutableSet<Throwable>, indent: String) {
+        fun <T> List<T>.indexOfFirstIndexed(predicate: (Int, T) -> Boolean): Int? {
+            for (i in this.indices) {
+                if (predicate(i, this[i])) {
+                    return i
+                }
+            }
+            return null
+        }
+
+        fun lastDivergingBetween(parent: List<StackTraceElement>, child: List<StackTraceElement>): Int? {
+            val reversedParent = parent.asReversed()
+            val reversedChild = child.asReversed()
+
+            val firstDiverging = reversedChild.indexOfFirstIndexed { i, element -> element != reversedParent.getOrNull(i) } ?: return null
+
+            return child.lastIndex - firstDiverging
+        }
+
+        fun Throwable.appendEnclosed(
+            target: MutableList<String>,
+            parentStack: List<StackTraceElement>,
+            seen: MutableSet<Throwable>,
+            header: String,
+            indent: String
+        ) {
+            if (!seen.add(this)) {
+                target += "    [circular reference to ${this::class.qualifiedName}]"
+                return
+            }
+
+            val thisStack = this.stackTrace.toList()
+            val lastDivergingBetweenIndex = lastDivergingBetween(parentStack, thisStack)
+
+            this.appendNameMessage(target, header, indent)
+
+            if (lastDivergingBetweenIndex != null) {
+                (0..lastDivergingBetweenIndex).forEach { thisStack[it].appendTo(target, indent) }
+            }
+
+            val commonFrames = thisStack.lastIndex - (lastDivergingBetweenIndex ?: -1)
+            if (commonFrames > 0) {
+                target += "$indent... $commonFrames more"
+            }
+
+            this.appendAllEnclosed(target, seen, indent)
+        }
+
+        val stackTrace = this.stackTrace.toList()
+        this.suppressed.forEach { it.appendEnclosed(target, stackTrace, seen, "Suppressed: ", "$indent    ") }
+        this.cause?.appendEnclosed(target, stackTrace, seen, "Caused by: ", indent)
+    }
+
+    fun Throwable.appendStackTrace(target: MutableList<String>, showStackTrace: Boolean) {
+        if (!showStackTrace) return
+
+        val seen = mutableSetOf(this)
+        this.stackTrace.forEach { it.appendTo(target, "") }
+        this.appendAllEnclosed(target, seen, "")
+    }
+
+    fun buildMessages(throwable: Throwable?, showStackTrace: Boolean, messageProvider: () -> String): List<String> {
+        return buildList {
+            add(messageProvider())
+            if (throwable != null) {
+                throwable.appendNameMessage(this, null, "")
+                throwable.appendStackTrace(this, showStackTrace)
+            }
+        }
+    }
+
+    fun List<String>.printMessagesOn(
+        error: Boolean,
+        normalPrinter: (String) -> Unit,
+        errorPrinter: (String) -> Unit,
+        messageEditor: (String) -> String = { it }
+    ) {
+        if (error) {
+            this.forEach { errorPrinter(messageEditor(it)) }
+        } else {
+            this.forEach { normalPrinter(messageEditor(it)) }
+        }
+    }
+
+    val messages = buildMessages(throwable, showStackTrace, messageProvider)
+    if (marker != null) {
+        messages.printMessagesOn(error, this::raw, this::rawError) { "$marker $it" }
+    } else {
+        messages.printMessagesOn(error, this::answer, this::raiseError)
+    }
 }
 
 internal fun Console.raw(message: String, newLine: Boolean = true) {
--- cli/src/main/kotlin/net/thesilkminer/babelk/cli/Logging.kt
+++ cli/src/main/kotlin/net/thesilkminer/babelk/cli/Logging.kt
@@ -34,102 +34,16 @@ internal class CliLoggerFactory : LoggerFactory {
 private class CliLogger(private val name: String, private val minLevel: LogLevel?, private val showStackTraces: Boolean) : Logger {
     override fun log(level: LogLevel, throwable: Throwable?, messageProvider: () -> String) {
         if (this.minLevel == null || level >= this.minLevel) return
-        val messages = this.buildMessages(throwable, messageProvider)
-        val printer = level.printer
-        messages.forEach { printer("${level.key} ${this.name}: $it") }
+        Console.log(throwable, level.isError, level.marker, this.showStackTraces) { "$name: ${messageProvider()}" }
     }
 
-    private fun buildMessages(throwable: Throwable?, messageProvider: () -> String): List<String> {
-        return buildList {
-            add(messageProvider())
-            if (throwable != null) {
-                throwable.appendNameMessage(this, null, "")
-                throwable.appendStacktrace(this)
-            }
-        }
-    }
+    private val LogLevel.isError: Boolean get() = this.ordinal <= LogLevel.WARN.ordinal
 
-    private fun Throwable.appendStacktrace(target: MutableList<String>) {
-        if (!this@CliLogger.showStackTraces) return
-
-        val seen = mutableSetOf(this)
-        this.stack.forEach { it.appendTo(target, "") }
-        this.appendAllEnclosed(target, seen, "")
-    }
-
-    private fun Throwable.appendNameMessage(target: MutableList<String>, header: String?, indent: String) {
-        target += "$indent${header ?: ""}${this::class.qualifiedName}: ${this.message ?: "<no error message>"}"
-    }
-
-    private fun Throwable.appendAllEnclosed(target: MutableList<String>, seen: MutableSet<Throwable>, indent: String) {
-        fun <T> List<T>.indexOfFirstIndexed(predicate: (Int, T) -> Boolean): Int? {
-            for (i in this.indices) {
-                if (predicate(i, this[i])) {
-                    return i
-                }
-            }
-            return null
-        }
-
-        fun lastDivergingBetween(parent: List<StackTraceElement>, child: List<StackTraceElement>): Int? {
-            val reversedParent = parent.asReversed()
-            val reversedChild = child.asReversed()
-
-            val firstDiverging = reversedChild.indexOfFirstIndexed { i, element -> element != reversedParent.getOrNull(i) } ?: return null
-
-            return child.lastIndex - firstDiverging
-        }
-
-        fun Throwable.appendEnclosed(
-            target: MutableList<String>,
-            parentStack: List<StackTraceElement>,
-            seen: MutableSet<Throwable>,
-            header: String,
-            indent: String
-        ) {
-            if (!seen.add(this)) {
-                target += "    [circular reference to ${this::class.qualifiedName}]"
-                return
-            }
-
-            val thisStack = this.stack
-            val lastDivergingBetweenIndex = lastDivergingBetween(parentStack, thisStack)
-
-            this.appendNameMessage(target, header, indent)
-
-            if (lastDivergingBetweenIndex != null) {
-                (0..lastDivergingBetweenIndex).forEach { thisStack[it].appendTo(target, indent) }
-            }
-
-            val commonFrames = thisStack.lastIndex - (lastDivergingBetweenIndex ?: -1)
-            if (commonFrames > 0) {
-                target += "$indent... $commonFrames more"
-            }
-
-            this.appendAllEnclosed(target, seen, indent)
-        }
-
-        this.suppressed.forEach { it.appendEnclosed(target, this.stack, seen, "Suppressed: ", "$indent    ") }
-        this.cause?.appendEnclosed(target, this.stack, seen, "Caused by: ", indent)
-    }
-
-    private fun StackTraceElement.appendTo(target: MutableList<String>, indent: String) {
-        target += "$indent    at $this"
-    }
-
-    private val Throwable.stack: List<StackTraceElement> get() = this.stackTrace.toList()
-
-    private val LogLevel.key: String
+    private val LogLevel.marker: Char?
         get() = when (this) {
-            LogLevel.ERROR -> "E"
-            LogLevel.WARN -> "W"
-            LogLevel.INFO -> "I"
-            LogLevel.DEBUG -> "D"
-        }
-
-    private val LogLevel.printer: (String) -> Unit
-        get() = when (this) {
-            LogLevel.ERROR, LogLevel.WARN -> Console::rawError
-            LogLevel.INFO, LogLevel.DEBUG -> Console::raw
+            LogLevel.ERROR -> null
+            LogLevel.WARN -> 'W'
+            LogLevel.INFO -> null
+            LogLevel.DEBUG -> 'D'
         }
 }
